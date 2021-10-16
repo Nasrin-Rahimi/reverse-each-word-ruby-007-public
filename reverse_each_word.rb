12d7a514c785e117d898cf63e1c802e31cefc230
Nasrin-Rahimi <77.nasrin@gmail.com>
2021-10-16 09:46:45 -0400

    reverse word by two pointer

--- reverse_each_word.rb
+++ reverse_each_word.rb
@@ -1,6 +1,31 @@
 def reverse_each_word(sentence)
-  reversed = sentence.split(" ").collect do |word|
-  word.reverse
+  sentence_array = sentence.split(" ").collect do |word|
+    word.reverse
+  end
+  
+  sentence_array.join(" ")
 end
-  reversed.join(" ")
+
+def reverse_word(word)
+  p1 = 0
+  p2 = word.length - 1
+
+  while p1 < p2
+    tmp = word[p1]
+    word[p1] = word[p2]
+    word[p2] = tmp
+    p1 += 1
+    p2 -= 1
+  end
+
+  word 
+  
 end
+# reverse without using ruby reverse method
+# def reverse_each_word(sentence)
+#   sentence_array = sentence.split(' ')
+#   sentence_array.collect do |word|
+#     reverse_word(word)
+#   end
+#   sentence_array.join(' ')
+# end
